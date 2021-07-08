8c6338620da2d41c33258552a7bc1bdece146b35
TomOldershaw <oldershaw.tom@gmail.com>
2021-07-08 16:27:09 +0100

    Improved dictionary

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -23,7 +23,7 @@ candidates_list = list(set(candidates))
 #Total votes
 total_votes = len(candidates)
 #Define dictionary to hold voting numbers
-candidate_votes = {"Votes": total_votes}
+candidate_votes = {}
 #Add candidates to dictionary
 for candidate in candidates_list:
     candidate_votes[candidate]=0
@@ -35,7 +35,8 @@ for candidate in candidates:
     candidate_votes[candidate] = candidate_votes[candidate] + 1
 
 #Sort by votes
-sorted_candidates = sorted(candidate_votes.items(), key=lambda x: x[1])
+sorted_candidates = sorted(candidate_votes.items(), key=lambda x: x[1], reverse=True)
+winner = sorted_candidates[0]
 
 #write output
 output = "Election Results\n"
@@ -43,9 +44,10 @@ output = output + "--------------------\n"
 output = output + "Total Votes: " + str(total_votes) + "\n"
 output = output + "--------------------\n"
 for (cand, votes) in sorted_candidates:
-    output = output + cand +": " + str(round(votes/total_votes,4)) + "% (" + str(votes) + ")\n"
+    output = output + cand +": " + str(round(votes*100/total_votes,4)) + "% (" + str(votes) + ")\n"
 output = output + "--------------------\n"
-#output = output + "Winner: " = sorted_candidates[0]
+#for (cand, votes) in winner:
+#    output = output + "Winner: " + str(cand)
 output = output + "--------------------\n"
 
 #Print to terminal
@@ -53,5 +55,6 @@ print(candidates_list)
 print(candidate_votes)
 print(sorted_candidates)
 print(output)
+print(winner)
 
 #Print to text file
