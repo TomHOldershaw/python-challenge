23637817efb551c3cb00936dbe873f3e470bc088
TomOldershaw <oldershaw.tom@gmail.com>
2021-07-08 15:10:12 +0100

    Commented python file

---
+++ PyPoll/main.py
@@ -0,0 +1,17 @@
+#Import dependencies
+
+#Setup lists etc
+
+#Open file and read header
+
+#Process data
+#List of candidates
+
+#Votes for each candidate
+#N.B. Results not required by county
+
+#Calculate results
+
+#Print to terminal
+
+#Print to text file
